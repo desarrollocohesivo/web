47daf3583b62fb8f31d775822afa87868fb9a7f3
stephaniaoz <stephania.osorio@correounivalle.edu.co>
2017-07-10 10:12:39 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+include_once("C:\wamp64\www\COHESIVO\utilidades\utilities.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php headPrincipal(); ?>
+		<title>Cohesivo</title>
+	</head>	
+	<body>
+		<?php menuPrincipal(); ?>
+		<div class="jumbotron text-center jumbotronprincipal">
+		  <img src="img/logoprincipal.png" alt="cohesivo" width="200px" height="200px">
+		  <h1>Desarrollo Cohesivo</h1>
+		  <p>Programamos más que soluciones, programamos un estilo de vida.</p> 
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="iconosprincipal">
+						<h3>Desarrollo web</h3>					
+						<img src="img/webdispositivos.png" width="128" height="128">
+					</div>	
+					<p class="texticonprincipal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda expedita iure possimus sit. Dolorem exercitationem quidem natus, rem quis, animi assumenda esse enim laborum itaque accusamus, eos necessitatibus ad id.</p>
+				</div>
+				<div class="col-sm-3">
+					<div class="iconosprincipal">
+						<h3>Software de escritorio</h3>
+						<img src="img/pcescritorio.png" width="128" height="128">
+					</div>						
+					<p class="texticonprincipal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus dolore, recusandae temporibus quam itaque eum quaerat fugit. Culpa debitis odio optio repellendus, ipsum, aperiam iure id accusantium, maxime neque natus.</p>
+				</div>
+				<div class="col-sm-3">
+					<div class="iconosprincipal">
+						<h3>Desarrollo android</h3>					
+						<img src="img/apandroid.png" width="128" height="128">
+					</div>	
+					<p class="texticonprincipal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores harum quia earum repudiandae omnis placeat quod voluptates, cupiditate facere, optio eveniet! Quas harum nam corporis totam eaque, tempora veniam voluptatum?</p>
+				</div>
+				<div class="col-sm-3">
+					<div class="iconosprincipal">
+						<h3>Web interactiva</h3>
+						<img src="img/bdweb.png" width="128" height="128">
+					</div>	
+					<p class="texticonprincipal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste debitis corporis molestias, explicabo accusamus ab harum nulla sequi mollitia eius, non maiores, distinctio dolorum obcaecati magni ut et nemo maxime.</p>
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>   
+	    <div class="copy">
+	        <div class="container">
+	            <p class="textcopy">Copyright © 2017 Cohesivo. Todos los derechos reservados. </p>
+	        </div>
+	    </div>
+	</footer>
+</html>
